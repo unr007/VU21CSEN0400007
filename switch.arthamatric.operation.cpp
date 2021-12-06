6ad0bb4988b9eb1bc191a344efd651e3cf174e29
Nithin Udumala <95117281+NithinUdumala@users.noreply.github.com>
2021-12-06 17:44:43 +0530

    Update switch.arthamatric.operation.cpp

--- switch.arthamatric.operation.cpp
+++ switch.arthamatric.operation.cpp
@@ -1,21 +1,36 @@
-#include<stdio.h>
-main()
-{
-int a,b;
-printf("enter a,b value");
-scanf("%d%d",&a,&b);
-switch(a)
-{
-case '+' : printf("%d is the sum \n",a+b);
-case '-' : printf("%d is the differance \n",a-b);
-case '*' : printf("%d is the product \n",a*b);
-case '/' : printf("%d is the Quotient \n",a/b);
-case '%' : printf("%d is the reminder ",a%b);
-break;
-default : printf("%d is the sum \n",a+b);
- printf("%d is the differance \n",a-b);
- printf("%d is the product \n",a*b);
- printf("%d is the Quotient \n",a/b);
- printf("%d is the reminder ",a%b);
-}
+#include <stdio.h>
+
+int main() {
+    char operator;
+    double n1, n2;
+
+    printf("Enter an operator (+, -, *, /): ");
+    scanf("%c", &operator);
+    printf("Enter two operands: ");
+    scanf("%lf %lf",&n1, &n2);
+
+    switch(operator)
+    {
+        case '+':
+            printf("%.1lf + %.1lf = %.1lf",n1, n2, n1+n2);
+            break;
+
+        case '-':
+            printf("%.1lf - %.1lf = %.1lf",n1, n2, n1-n2);
+            break;
+
+        case '*':
+            printf("%.1lf * %.1lf = %.1lf",n1, n2, n1*n2);
+            break;
+
+        case '/':
+            printf("%.1lf / %.1lf = %.1lf",n1, n2, n1/n2);
+            break;
+
+        // operator doesn't match any case constant +, -, *, /
+        default:
+            printf("Error! operator is not correct");
+    }
+
+    return 0;
 }
